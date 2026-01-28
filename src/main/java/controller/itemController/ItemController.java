7ea08a6692c2d43598ec45051af987eb257c9153
Manesh Edirisinghe <edirisinghemadhura@gmail.com>
2026-01-28 09:19:43 +0530

    first commit

---
+++ src/main/java/controller/itemController/ItemController.java
@@ -0,0 +1,102 @@
+package controller.itemController;
+
+import db.DBConnection;
+import javafx.collections.ObservableList;
+import model.dto.ItemDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ItemController implements ItemService{
+    @Override
+    public void addItemDetails(String itemCode, String description, String PackSize, double unitPrice, int qtyOnHand) {
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+
+            String SQL = "INSERT INTO item VALUES(?, ?, ?, ?, ?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+            preparedStatement.setObject(1, itemCode);
+            preparedStatement.setObject(2, description);
+            preparedStatement.setObject(3, PackSize);
+            preparedStatement.setObject(4, unitPrice);
+            preparedStatement.setObject(5, qtyOnHand);
+
+            preparedStatement.executeUpdate();
+
+        } catch (
+                SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void deleteItemDetails(String itemCode) {
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement pstm = connection.prepareStatement("DELETE FROM item WHERE ItemCode = ?");
+
+            pstm.setObject(1,itemCode);
+            pstm.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    @Override
+    public void updateItemDetails(String itemCode, String description, String PackSize, double unitPrice, int qtyOnHand) {
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+
+            String SQL = "UPDATE item SET Description = ?, PackSize = ?, UnitPrice = ?, QtyOnHand = ? WHERE ItemCode = ?";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+            preparedStatement.setObject(1, description);
+            preparedStatement.setObject(2, PackSize);
+            preparedStatement.setObject(3, unitPrice);
+            preparedStatement.setObject(4, qtyOnHand);
+            preparedStatement.setObject(5, itemCode);
+
+            preparedStatement.executeUpdate();
+
+        } catch (
+                SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public ObservableList<ItemDTO> getAllItemDetails() {
+        ObservableList<ItemDTO> itemDetails = javafx.collections.FXCollections.observableArrayList();
+
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            String SQL = "SELECT * FROM item";
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                itemDetails.add(new ItemDTO(
+                        resultSet.getString("ItemCode"),
+                        resultSet.getString("Description"),
+                        resultSet.getString("PackSize"),
+                        resultSet.getDouble("UnitPrice"),
+                        resultSet.getInt("QtyOnHand")
+                ));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return itemDetails;
+    }
+}
---
+++ src/main/java/controller/itemController/ItemService.java
@@ -0,0 +1,15 @@
+package controller.itemController;
+
+import javafx.collections.ObservableList;
+import model.dto.ItemDTO;
+
+public interface ItemService {
+
+    void addItemDetails(String itemCode, String description, String PackSize, double unitPrice, int qtyOnHand);
+
+    void deleteItemDetails(String itemCode);
+
+    void updateItemDetails(String itemCode, String description, String PackSize, double unitPrice, int qtyOnHand);
+
+    ObservableList<ItemDTO> getAllItemDetails();
+}
